83f6c9e39a1064a95d4d423e6834c6f33207200f
basitmushtaq <basitmushtaq78@gmail.com>
2018-07-29 20:25:05 +0530

    added by Basit

--- Strings/strchr.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char mystr[30] = "I’m an example of function strchr";
-    printf ("%s", strchr(mystr, 'f'));
-    return 0;
-}
--- Strings/strlen.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char str1[20] = "BeginnersBook";
-    printf("Length of string str1: %d", strlen(str1));
-    return 0;
-}
